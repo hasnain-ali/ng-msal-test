a1b80682862bbba4a79f7474697bf708356e8cb9
Hasnain Ali <Hasnain.Ali@Achilles.com>
2021-02-08 01:36:42 +0000

    Updated with MSAL and working with Login Popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MsalModule } from '@azure/msal-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MsalModule.forRoot({
+      auth:{
+        clientId: "b13c4ebf-ec9c-4feb-931f-96f8ad09be2a",
+        authority: "https://contosob2chatest.b2clogin.com/contosob2chatest.onmicrosoft.com/B2C_1_sin_sup",
+        validateAuthority: false,
+        redirectUri: "http://localhost:4200/"
+      },
+      cache:{
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: false
+      }
+    },{
+      consentScopes:[
+        "user.read","openid","profile"
+      ]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
